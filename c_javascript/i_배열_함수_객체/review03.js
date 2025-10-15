a4793ad3c334817ecffbb243752950340b6d1248
Kitbomin <zzxxcc1433@naver.com>
2025-10-15 15:26:07 +0900

    review03 - Library System

---
+++ c_javascript/i_배열_함수_객체/review03.js
@@ -0,0 +1,197 @@
+
+// 도서관 관리 시스템 //
+// : 도서관의 책 관리 시스템을 구현
+
+//! 프로젝트 데이터 정의
+/**
+ * 1) 도서관 - 객체
+ *    속성: 여러 도서
+ *    기능: 도서 추가, 도서 목록 출력, 특정 도서 대여, 특정 도서 반납
+ * 
+ * 2) 도서(책) - 객체
+ *    속성: 책 고유 ID, 책 제목, 책 저자, 책 대여 가능 여부
+ */
+
+let exampleLibrary = {
+  books: [],
+
+}
+
+let exampleBook = {
+  id: 1,
+  title: '책 목록',
+  author: '책 저자',
+  isAvailable: true     //기본값
+}
+
+//! === 프로젝트 구현 ===
+
+//? Book 클래스: 각 책의 정보 저장 및 대여, 반납 기능 정의
+class Book {
+
+  // 자바같으면 이렇게 했다 진짜
+  // let id;
+  // let title;
+  // let author;
+  // let isAvailable
+
+  // 생성자 함수(메서드)
+  constructor(id, title, author) {
+    // 생성자 함수에서 this 키워드로 속성 정의되는 값(좌항)은 필드(우항)로 자동 정의됨
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.isAvailable = true;
+  }
+
+  // +) 책 내부 기능 명시
+  // - 대여 기능
+  // 객체명.rentBook(); <- 이렇게 호출 될거임
+  // : 메서드를 호출한 해당 책(객체)의 대여를 의미함
+  rentBook() {
+    if(this.isAvailable) {
+      // 책 대여가 가능함
+      this.isAvailable = false;
+      console.log(`${this.title}이(가) 성공적으로 대여되었습니다.`);
+    }
+  }
+
+  // - 반납 기능
+  returnBook() {
+    if(!this.isAvailable) {
+      this.isAvailable = true;
+      console.log(`${this.title}이(가) 성공적으로 반납되었습니다.`);
+    }
+  }
+
+}
+
+//? Library 클래스: Book 객체의 목록관리 및 추가 기능 구현(출력, 대여/반납 기능)
+class Library {
+  constructor() {
+    this.books = [];      // 초기에는 책이 없음 (빈 배열)
+    this.nextBookId = 1;  // 책 고유 값
+  }
+
+  // +) 클래스에서의 메서드 정의 시 함수의 형태로 사용됨 
+  //  : function 키워드가 부재됨
+
+
+  // 책 생성
+  addBook(title, author) {
+    const newBook = new Book(this.nextBookId, title, author);
+    
+    // this(Library 객체).books(객체의 속성-배열).push(객체);
+    this.books.push(newBook);
+
+    console.log(`${title} 책이 도서관에 추가되었습니다. (저자: ${author})`);
+
+    this.nextBookId++;
+  }
+
+  // 책 조회
+  displayBook() {
+    console.log('=== Library ===');
+    this.books.forEach(book => {
+      console.log(`${book.id}: ${book.title} - by ${book.author} 
+                    - ${book.isAvailable ? '대여 가능' : '불가능'}`);
+    })
+  }
+
+  // 빌린 책 호출
+  rentBook(id) {
+    // +) findIndex: 조건에 일치하는 요소의 index를 반환  - 반환 타입: 숫자
+    //      >> 찾는 요소가 없으면 -1 반환
+    //    find:      조건에 일치하는 요소 그 자체를 반환 (객체 반환할 때 좋겠네) - 반환 타입: 객체
+    //      >> 찾는 요소가 없으면 undifined 반환
+    const book = this.books.find(book => book.id === id);
+
+    if(book) {
+      // 해당 책이 도서관에 등록 되어있는 경우 - 책 객체 내부의 메서드 호출
+      book.rentBook();
+    } else {
+      // 책이 없는 경우
+      console.log('해당하는 책을 찾을 수 없습니다.');
+    }
+  }
+
+  // 반납된 책 호출
+  returnBook(id) {
+    const book = this.books.find(book => book.id === id);
+
+    if(book) {
+      // 해당 책이 도서관에 등록된 경우 (책 반납 여부는 미정임)
+      book.returnBook();
+    } else {
+      console.log('해당하는 책을 찾을 수 없습니다.');
+    }
+
+  }
+
+  // 책 업데이트
+  updateBook(id, newTitle, newAuthor) {
+    // +) find 메서드는 배열에서 제공된 콜백함수를 만족하는 첫번째 요소를 반환함
+    //    만약에 this.books.find(book => book.title.include("신데렐라"));
+    const book = this.books.find(book => book.id === id);
+
+    if(!book) {
+      console.log('해당 책을 찾을 수 없습니다.');
+      return;
+    }
+
+    const isNewTitleValid = newTitle && newTitle.trim().length>0;
+    const isNewAuthorValid = newAuthor && newAuthor.trim().length>0;
+
+
+    if(!isNewTitleValid && !isNewAuthorValid) {
+      console.log('제목 또는 저자 중 하나는 반드시 수정되어야 합니다.');
+      console.log('수정할 데이터가 없어요');
+      return;
+    }
+
+    book.title = newTitle || book.title;
+    book.author = newAuthor || book.author;
+  
+    console.log(`책 (id: ${book.id})정보가 업데이트 되었습니다. : 제목 - ${book.title}, 저자 - ${book.author}`);
+  }
+
+  // 책 삭제
+  removeBook(id) {
+    const idx = this.books.findIndex(book => book.id === id);
+    if(idx !== -1) {
+      const removeBook = this.books.splice(idx, 1)
+      // const [removeBook] = this.books.splice(idx, 1);
+
+      console.log(`${removeBook.title} (id: ${removeBook.id}) 책이 도서관에서 삭제되었습니다.`);
+    } else {
+      console.log('해당 책을 찾을 수가 없어요');
+    }
+  }
+
+
+  // +) 추가 기능 구현
+  // [필터링] 저자별 도서 필터링
+
+  // [필터링] 제목 키워드로 도서 검색
+
+  // [필터링] 대여 가능 여부로 도서 필터링
+
+  // [통계] 대여 가능 도서 수 집계
+
+
+}
+
+const busanLibrary = new Library();
+busanLibrary.addBook('자스 진짜 근본 없다', 'Ara');
+busanLibrary.addBook('C는 진짜 근본인데 너무 원초적이다', 'Silbia');
+busanLibrary.addBook('Java 가 진짜 재밌는데', 'Ara');
+busanLibrary.addBook('근데 자바 너무 어려움', 'Silbia');
+busanLibrary.addBook('C#도 살짝 근본잃은 자바 느낌', 'Ara');
+
+busanLibrary.rentBook(1);
+busanLibrary.displayBook();
+busanLibrary.rentBook(1);
+
+busanLibrary.returnBook(1);
+busanLibrary.displayBook();
+busanLibrary.rentBook(10);
