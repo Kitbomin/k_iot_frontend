8f6ee49c4278651e8b6bbed825f4179e77ada1d2
Kitbomin <zzxxcc1433@naver.com>
2025-10-28 16:06:44 +0900

    when click window, close modal

--- e_typescript/src/z_종합예제/c_modal/실습.ts
+++ e_typescript/src/z_종합예제/c_modal/실습.ts
@@ -70,53 +70,53 @@ const displayUsers = (users: UsersType) => {
 
 // 4. 사용자 정보를 받아 모달 창에 표시하는 함수
 const showModal = (user: User) => {
-  const modal = document.getElementById('user-modal');
-  const modalContent = document.getElementById('modal-user-details');
+  //* modal을 들고온다 -> 'user-modal' 에서
+  //* modalContent를 들고온다 -> 'modal-user-details'에서
 
-  if (modal && modalContent) {
-    modalContent.innerHTML = `
-      <h2>${user.name}</h2>
-      <p><strong>UserName: </strong> ${user.username}</p>
-      <p><strong>Email: </strong> ${user.email}</p>
-      <p><strong>Phone: </strong> ${user.phone}</p>
-      <p><strong>Website: </strong> ${user.website}</p>
-    `;
+  //* 만약 모달과 모달컨텐츠가 다 존재한다면
+    //* 다음의 내용을 넣는다
+    // modalContent.innerHTML = `
+    //   <h2>${user.name}</h2>
+    //   <p><strong>UserName: </strong> ${user.username}</p>
+    //   <p><strong>Email: </strong> ${user.email}</p>
+    //   <p><strong>Phone: </strong> ${user.phone}</p>
+    //   <p><strong>Website: </strong> ${user.website}</p>
+    // `;
 
-    modal.style.display = 'block'; //모달창 표시
-  }
+    //* 그리고 해당 모달의 스타일을 표시한다
+    // modal.style.display = 'block'; //모달창 표시
+  
 }
 
 // 5. 사용자 리스트에 이벤트 리스너 추가
-const addEventListeners = (users: UsersType) => {
-  const userList = document.getElementById('user-list');
+const addEventListener = (users: UsersType) => {
+  //* userList를 가져온다 'user-list'에서
 
-  if (userList) {
-    userList.addEventListener('click', (e) => { 
+  //* 만약 userList가 있다면
+    //* userList에 클릭 이벤트를 전달한다 
       //? cf) target VS current Target
       //    - target: 진짜 이벤트가 처음발생하는 DOM요소 (클릭이 일어난 요소)
       //    - current Target: 발생한 이벤트가 등록된 DOM 요소 (이벤트가 바인딩 된)
-
-      const target = e.target as HTMLElement;
+      
+      //* 클릭이 발생하는 타겟의 속성을 HTMLElement로 정의한다
       // >> 클릭이 발생한 요소는 card 내부의 h2, p가 될 가능성이 존재함 그래서 상위요소인 HTMLElement를 선언함
 
-      const userCard = target.closest('.user-card') as HTMLElement | null;
+      //* userCard를 선언한다 -> target과 가장 가까운 .user-card를 -> 그리고 그걸 HTMLElement 속성 또는 null 로 선언한다 
       // : 이벤트가 발생한 요소와 가장 가까운 .user-card 요소를 반환해줄거임
 
-      if (userCard) {
+      //* 만약 userCard가 존재한다면?
         // dataset: userCard가 가진 data-* 속성들에 접근할 수 있는 DOM 속성
         // ex) <div data-role="admin" data-userId="1"></div>
 
         // parseInt(): 숫자로 변환할 데이터, 변환 진수
-        const userId = parseInt(userCard.dataset.userId || '0', 10);
+        //* userId를 가져온다 -> parseInt를 써서 dataset에 있는 userId를 10진수로 바꿔서
 
-        const user = users.find(user => user.id === userId);
+        //* user를 가져온다 -> users에서 찾아서 -> user 객체마다 userId랑 일치하는지 확인해서
+        
+        //* 만약 user가 있다면
+          //* showModal에 user를 넣어 호출한다
 
-        if (user) {
-          showModal(user);
-        }
-      }
-    });
-  }
+
 
   const modal = document.getElementById('user-modal') as HTMLElement;
   const closeModal = document.querySelector('.close') as HTMLElement;
@@ -124,16 +124,6 @@ const addEventListeners = (users: UsersType) => {
   if (modal && closeModal) {
     closeModal.addEventListener('click', () => {
       modal.style.display = 'none';
-    });
-
-    // 브라우저 전체를 DOM 요소로 반환(브라우저 탭의 전체 영역)
-    // : window
-
-    // cf) document: window에 로드되는 HTML 문서 그 자체
-    window.addEventListener('click', (e) => {
-      if (e.target === modal) {
-        modal.style.display = 'none';
-      }
     })
   }
 }
@@ -141,7 +131,7 @@ const addEventListeners = (users: UsersType) => {
 const init = async () => {
   const users = await fetchUsers();
   displayUsers(users);
-  addEventListeners(users);
+  addEventListener(users);
 }
 
 document.addEventListener('DOMContentLoaded', init);
